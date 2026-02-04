783e9ec75dd784eeee16428b2971382bb81b7b63
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-02-04 17:52:53 +0100

    Did fibN, minor changes

--- S02/fibN.py
+++ S02/fibN.py
@@ -2,11 +2,10 @@ def fibonacci(n):
     a = 0
     b = 0
     c = 1
-    print(c) #this one is needed for f(0)
     for i in range (1,n+1):
         a = b
         b = c
         c = a + b
-        print(c)
+    return c
 
-fibonacci(4)
+print(fibonacci(0))
