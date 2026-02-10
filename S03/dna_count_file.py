58f704492b2c11939b6a2ec705efa84b59792065
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-02-10 16:40:28 +0100

    Made dna_count_file.py and .txt

---
+++ S03/dna_count_file.py
@@ -0,0 +1,27 @@
+def txt_read(path):
+    file = open(path,"r")
+    out = file.read().split("\n")
+    file.close()
+    return out
+
+line_n = 0
+for seq in txt_read("dna_count_file.txt"):
+    line_n += 1
+    bases_a = 0
+    bases_c = 0
+    bases_t = 0
+    bases_g = 0
+    if len(seq) != 0:
+        for base in seq:
+            base = base.lower()
+            if base == "a":
+                bases_a += 1
+            elif base == "c":
+                bases_c += 1
+            elif base == "t":
+                bases_t += 1
+            elif base == "g":
+                bases_g += 1
+            else:
+                pass
+        print("Line " + str(line_n) + " contains: A-" + str(bases_a) + ", C-" + str(bases_c) + ", T-" + str(bases_t) + ", G-" + str(bases_g))
