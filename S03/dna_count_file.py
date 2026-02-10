7e146dfd417e0a946835ac8831f131e8317b7d17
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-02-10 16:53:11 +0100

    Optimised the code

--- S03/dna_count_file.py
+++ S03/dna_count_file.py
@@ -1,5 +1,5 @@
 def txt_read(path):
-    file = open(path,"r")
+    file = open("S03/" + path,"r")
     #out = file.read().split("\n")
     out = file.readlines()
     file.close()
@@ -8,21 +8,12 @@ def txt_read(path):
 line_n = 0
 for seq in txt_read("dna_count_file.txt"):
     line_n += 1
-    bases_a = 0
-    bases_c = 0
-    bases_t = 0
-    bases_g = 0
-    if len(seq) != 0:
-        for base in seq:
-            base = base.lower()
-            if base == "a":
-                bases_a += 1
-            elif base == "c":
-                bases_c += 1
-            elif base == "t":
-                bases_t += 1
-            elif base == "g":
-                bases_g += 1
-            else:
-                pass
-        print("Line " + str(line_n) + " contains: A-" + str(bases_a) + ", C-" + str(bases_c) + ", T-" + str(bases_t) + ", G-" + str(bases_g))
+    bases = {"A":0,"C":0,"T":0,"G":0}
+    if seq[0] != "\n":
+        for e in seq:
+            if e in bases:
+                bases[e] += 1
+        mesg = "Line " + str(line_n) + " contains: "
+        for e in bases:
+            mesg = mesg + " " + e + "-" + str(bases[e]) 
+        print(mesg)
