d05e92330477844f73f7b82494e6f1faa6b9258c
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-02-11 18:21:39 +0100

    basic exercises added

---
+++ Basic_Exercises/1.py
@@ -0,0 +1,8 @@
+dna = "TGGCCATTGTAATGGGCCGCTGAAAG"
+#lower, count, replace
+print(len(dna))
+print(dna[0:5])
+print(dna[-4:])
+print(dna.lower())
+print(dna.count("ATG"))
+print(dna.replace("T","U"))
---
+++ Basic_Exercises/2.py
@@ -0,0 +1,10 @@
+txt = "  Hello, World! Welcome to Python Programming  "
+
+print(txt)
+txt_strp = txt.strip()
+print(txt_strp)
+print(len(txt.split(" ")))
+print(txt_strp.title())
+print(txt_strp.startswith("Hello"))
+print(txt_strp.endswith("ing."))
+print(txt_strp.find("Python"))
