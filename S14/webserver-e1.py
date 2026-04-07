a1da59ea3e4ee2cb6a69c6e1447a7a509773c904
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-04-07 17:09:57 +0200

    Exercises of S14

---
+++ S14/webserver-e1.py
@@ -0,0 +1,60 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8080
+PATH = "./S14/pages/"
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        print("GET received! Request line:")
+
+        # Print the request line
+        termcolor.cprint("  " + self.requestline, 'green')
+
+        # Print the command received (should be GET)
+        print("  Command: " + self.command)
+        
+        if self.path == "/":
+            self.send_response(200)
+            contents = "Welcome to my server"
+        else:
+            self.send_response(404)
+            contents = "Resource not available"
+
+        termcolor.cprint(self.requestline, 'green')
+
+        # Define the content-type header:
+        self.send_header('Content-Type', "text/plain")
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT) # -- Main loop: Attend the client. Whenever there is a new client, the handler is called
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
---
+++ S14/webserver-e3.py
@@ -0,0 +1,72 @@
+import http.server
+import socketserver
+import termcolor
+
+# Define the Server's port
+PORT = 8080
+PATH = "./S14/pages/"
+
+class TestHandler(http.server.BaseHTTPRequestHandler):
+    
+    def do_GET(self):
+        print("GET received! Request line:")
+
+        # Print the request line
+        termcolor.cprint("  " + self.requestline, 'green')
+
+        # Print the command received (should be GET)
+        print("  Command: " + self.command)
+
+        try:
+            if self.path == "/":
+                page = open(PATH + "/index.html")
+            else:
+                page = open(PATH + self.path)
+                
+            contents = page.read()
+            page.close()
+            
+            response_code = 200
+        except FileNotFoundError:
+            page = open(PATH + "/error.html")
+            contents = page.read()
+            page.close()
+            response_code = 404
+        
+        self.send_response(response_code)
+        style = "text/html"
+
+        termcolor.cprint(self.requestline, 'green')
+
+        # Define the content-type header:
+        self.send_header('Content-Type', style)
+        self.send_header('Content-Length', len(contents.encode()))
+
+        # The header is finished
+        self.end_headers()
+
+        # Send the response message
+        self.wfile.write(contents.encode())
+
+        return
+
+# -- This is for preventing the error: "Port already in use"
+socketserver.TCPServer.allow_reuse_address = True
+
+# ------------------------
+# - Server MAIN program
+# ------------------------
+# -- Set the new handler
+Handler = TestHandler
+
+# -- Open the socket server
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("Serving at PORT", PORT) # -- Main loop: Attend the client. Whenever there is a new client, the handler is called
+    
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        print("")
+        print("Stopped by the user")
+        httpd.server_close()
+
