b80d6253023983fa21068317bc8a1bf614933ca9
SCuadradoC <s.cuadradoc.2025@alumnos.urjc.es>
2026-04-21 13:49:58 +0200

    Final part of P06

--- P06/Seq2-Server.py
+++ P06/Seq2-Server.py
@@ -6,6 +6,20 @@ import os
 #from config import *
 import termcolor
 
+def insert_content(content:str, template:list, data:list):
+    if type(template) != list:
+        template = [template]
+    if type(data) != list:
+        data = [data]
+    if len(template) != len(data):
+        return
+    print(template)
+    print(data)
+    for n in range(0,len(template)):
+        content = content.replace(f"[[{template[n]}]]",data[n])
+    return content
+
+
 # Define the Server's port
 IP = "127.0.0.1" #socket.gethostbyname(socket.gethostname())
 PORT = 8080
@@ -41,8 +55,6 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 page = open(PATH + "/index.html")
                 contents = page.read()
                 page.close()
-                
-                contents = contents.replace("[[lnk]]", LNK)
                 options = ""
                 for e in range(0,len(seqs)):
                     options += f'<option value="{e}">{e}</option> '
@@ -53,36 +65,67 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
                 contents = contents.replace("[[options_gene]]",options)
 
                 style = "text/html"
-            elif self.path == "/logo.png":
-                page = open(PATH + "favicon.ico", "rb")
-                contents = page.read()
-                style = "image/png"
+#            elif self.path == "/favicon.ico":
+#                page = open(PATH + "/logo.png", "rb")
+#                contents = page.read()
+#                style = "image/png"
 
             elif "/echo" in self.path:
                 print(self.path)
                 if "get_sequence" in self.path:
                     req_val = int(self.path.split("=")[1])
-                    contents = f"""<p>Sequence requested:</p> <p>{seqs[req_val]}</p> <a href="{LNK}">Main page</a>"""
+                    file = open(PATH + "/get.html")
+                    page_raw = file.read()
+                    file.close()
+                    contents = insert_content(page_raw,["n","seq_cont"],[str(req_val),seqs[req_val]])
                     style = "text/html"
                 elif "get_gene" in self.path:
                     req_val = self.path.split("=")[1]
                     file = open(genes[req_val])
                     gene_raw = file.read().split("\n")
                     file.close()
-                    gene_page = ""
+                    file = open(PATH + "/gene.html")
+                    page_raw = file.read()
+                    file.close()
+                    gene_proc = ""
                     for e in gene_raw:
-                        gene_page += f"{e}<br> "
-                    contents = f"""<p>Gene requested:</p> {gene_page} <a href="{LNK}">Main page</a>"""
+                        gene_proc += f"{e}<br> "
+                    contents = insert_content(page_raw,["gene_name","gene_cont"],[req_val,gene_proc])
                     file.close()
                     style = "text/html"
             elif "operation" in self.path:
-                req_val = self.path.split("=")[1]
-                #add code to generate the html
+                req_val = self.path.split("?")[1].split("&")
+                seq_in = Seq(req_val[0].split("=")[1],False)
+                operation = req_val[1].split("=")[1]
+
+                if operation == "Info":
+                    bases = seq_in.count()
+                    l = len(seq_in)
+                    if l != 0:
+                        result = f"""<p>Total length: {l}</p>
+<p>A: {bases["A"]} ({bases["A"] / l * 100}%)</p>
+<p>C: {bases["C"]} ({bases["C"] / l * 100}%)</p>
+<p>T: {bases["T"]} ({bases["T"] / l * 100}%)</p>
+<p>G: {bases["G"]} ({bases["G"] / l * 100}%)</p>"""
+                    else:
+                        seq_in = req_val[0].split("=")[1]
+                        result = "Erroneous sequence"
+                    
+                elif operation == "Complement":
+                    result = str(seq_in.complement())
+                elif operation == "Reverse":
+                    result = str(seq_in.reverse())
+                file = open(PATH + "/operation.html")
+                page_raw = file.read()
+                file.close()
+                
+                contents = insert_content(page_raw,["seq_in","operation","result"],[str(seq_in),operation,result])
+                style = "text/html"
             else:
                 page = open(PATH + self.path + ".html")
                 contents = page.read()
                 page.close()
-                contents = contents.replace("[[lnk]]", LNK)
+                
                 style = "text/html"
             response_code = 200
         except FileNotFoundError:
@@ -92,7 +135,7 @@ class TestHandler(http.server.BaseHTTPRequestHandler):
             style = "text/html"
             response_code = 404
         
-        
+        contents = contents.replace("[[lnk]]", LNK)
         self.send_response(response_code)
         
         termcolor.cprint(self.requestline, 'green')
