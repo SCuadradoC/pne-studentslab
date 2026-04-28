ccdd45bd0d5c0684a4a5634cbf113ee234744e5f
SCuadradoC <s.cuadradoc.2025@urjc.alumnos.es>
2026-04-28 16:40:17 +0200

    First part of P_Final

---
+++ P_Final/html_helper.py
@@ -0,0 +1,24 @@
+def insert_content(content:str, template:list, data:list):
+    if type(template) != list:
+        template = [template]
+    if type(data) != list:
+        data = [data]
+    if len(template) != len(data):
+        return
+    print(template)
+    print(data)
+    for n in range(0,len(template)):
+        content = content.replace(f"[[{template[n]}]]",data[n])
+    return content
+
+def parse_req(path:str):
+    n = 1
+    page = ""
+    param = {}
+    while path[0] != "?":
+        page += path.pop(0)
+    path.pop(0)
+    for e in path.split("&"):
+        i = e.split("=")
+        param.update({i[0]:i[1]})
+    return page,param
